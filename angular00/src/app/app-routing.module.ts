81e2f0cc5840b95b3c79a02516121ace5ee832e8
achaayb <achaayb@sekera-group.com>
2022-08-01 11:18:51 +0100

    add one, two modules

---
+++ angular00/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'one', loadChildren: () => import('./one/one.module').then(m => m.OneModule) }, { path: 'two', loadChildren: () => import('./two/two.module').then(m => m.TwoModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
