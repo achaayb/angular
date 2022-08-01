425b2ac8b6765002340b0cd11ba046a0510eccbc
achaayb <achaayb@sekera-group.com>
2022-08-01 11:28:47 +0100

    add reusable module

---
+++ angular00/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReusableComponent } from './../reusable/reusable.component';
+
+
+
+@NgModule({
+  declarations: [ReusableComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
